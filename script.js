795d53ab7ae5c9a29dafe4366b4bffde906b4b61
SaagiiSG <saranochir.s@gmaile.com>
2023-10-25 19:45:36 +0800

    last photo fix , test score for mobile device

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function home(){
     location.href = '#home';
 }
+var slider_img = document.querySelector('.slider-img');
+var images = ['../photos/6.5.png', '../photos/7.5-1.png', '../photos/7.5.png', '../photos/7.png', '../photos/8.png'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
